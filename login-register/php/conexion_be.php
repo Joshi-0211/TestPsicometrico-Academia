c9dc2280f1c91391b09dc6684792cbb6f37f953f
Joshua Davila <131328357+Joshi-0211@users.noreply.github.com>
2024-10-23 10:55:13 -0600

    Update conexion_be.php

--- login-register/php/conexion_be.php
+++ login-register/php/conexion_be.php
@@ -2,9 +2,9 @@
 
 $servidor = "ewr1.clusters.zeabur.com";  // Agregar punto y coma
 $usuario = "root";  // Agregar punto y coma
-$contrasena = "3I8Nhu40jFCTAqDmG7LSbZl126U95aRK";  // Agregar punto y coma
-$base_de_datos = "login_register_db";  // Agregar punto y coma
-$port = 30167;  // El puerto debe ser un número entero, sin comillas y sin los dos puntos
+$contrasena = "FBjsHQ1kP7WS9LolR53gUY6ci04weG82";  // Agregar punto y coma
+$base_de_datos = "zeabur";  // Agregar punto y coma 
+$port = 31318;  // El puerto debe ser un número entero, sin comillas y sin los dos puntos
 
 // Crear la conexión
 $conexion = mysqli_connect($servidor, $usuario, $contrasena, $base_de_datos, $port);
